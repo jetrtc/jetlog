a821eba3507cf9b526d9d2c3dd39d4756896f2c1
JetRTC <jetrtc@gmail.com>
2019-10-26 15:10:37 +0800

    Improve logger/loggable design

---
+++ loggable.go
@@ -0,0 +1,29 @@
+package log
+
+type Loggable struct {
+	Logger Logger
+}
+
+func (l Loggable) Output(level LogLevel, format string, v ...interface{}) {
+	l.Logger.Output(level, format, v...)
+}
+
+func (l Loggable) Fatalf(format string, v ...interface{}) {
+	l.Logger.Output(LogFatal, format, v...)
+}
+
+func (l Loggable) Debugf(format string, v ...interface{}) {
+	l.Logger.Output(LogDebug, format, v...)
+}
+
+func (l Loggable) Errorf(format string, v ...interface{}) {
+	l.Logger.Output(LogError, format, v...)
+}
+
+func (l Loggable) Infof(format string, v ...interface{}) {
+	l.Logger.Output(LogInfo, format, v...)
+}
+
+func (l Loggable) Warningf(format string, v ...interface{}) {
+	l.Logger.Output(LogWarning, format, v...)
+}
--- logger.go
+++ logger.go
@@ -16,11 +16,7 @@ const (
 )
 
 type Logger interface {
-	Fatalf(format string, v ...interface{})
-	Debugf(format string, v ...interface{})
-	Errorf(format string, v ...interface{})
-	Infof(format string, v ...interface{})
-	Warningf(format string, v ...interface{})
+	Output(level LogLevel, format string, v ...interface{})
 }
 
 func NewDefaultLogger(logger *log.Logger) *DefaultLogger {
@@ -33,7 +29,7 @@ type DefaultLogger struct {
 	Level     LogLevel
 }
 
-func (l *DefaultLogger) output(level LogLevel, format string, v ...interface{}) {
+func (l *DefaultLogger) Output(level LogLevel, format string, v ...interface{}) {
 	if level < l.Level {
 		return
 	}
@@ -51,48 +47,4 @@ func (l *DefaultLogger) output(level LogLevel, format string, v ...interface{}) 
 		label = "F"
 	}
 	l.logger.Output(l.Calldepth, fmt.Sprintf("[%s] %s", label, fmt.Sprintf(format, v...)))
-}
-
-func (l *DefaultLogger) Fatalf(format string, v ...interface{}) {
-	l.output(LogFatal, format, v...)
-}
-
-func (l *DefaultLogger) Debugf(format string, v ...interface{}) {
-	l.output(LogDebug, format, v...)
-}
-
-func (l *DefaultLogger) Errorf(format string, v ...interface{}) {
-	l.output(LogError, format, v...)
-}
-
-func (l *DefaultLogger) Infof(format string, v ...interface{}) {
-	l.output(LogInfo, format, v...)
-}
-
-func (l *DefaultLogger) Warningf(format string, v ...interface{}) {
-	l.output(LogWarning, format, v...)
-}
-
-type Loggable struct {
-	Logger Logger
-}
-
-func (l Loggable) Fatalf(format string, v ...interface{}) {
-	l.Logger.Fatalf(format, v...)
-}
-
-func (l Loggable) Debugf(format string, v ...interface{}) {
-	l.Logger.Debugf(format, v...)
-}
-
-func (l Loggable) Errorf(format string, v ...interface{}) {
-	l.Logger.Errorf(format, v...)
-}
-
-func (l Loggable) Infof(format string, v ...interface{}) {
-	l.Logger.Infof(format, v...)
-}
-
-func (l Loggable) Warningf(format string, v ...interface{}) {
-	l.Logger.Warningf(format, v...)
 }
