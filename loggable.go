af118bc3cb87dd3d8f8af0750bf7583e7f98a2c4
JetRTC <jetrtc@gmail.com>
2019-10-26 18:52:42 +0800

    Loggable revised

--- loggable.go
+++ loggable.go
@@ -4,26 +4,30 @@ type Loggable struct {
 	Logger Logger
 }
 
-func (l Loggable) Output(level LogLevel, format string, v ...interface{}) {
+func NewLoggable(logger Logger) *Loggable {
+	return &Loggable{Logger: logger}
+}
+
+func (l *Loggable) Output(level LogLevel, format string, v ...interface{}) {
 	l.Logger.Output(level, format, v...)
 }
 
-func (l Loggable) Fatalf(format string, v ...interface{}) {
+func (l *Loggable) Fatalf(format string, v ...interface{}) {
 	l.Logger.Output(LogFatal, format, v...)
 }
 
-func (l Loggable) Debugf(format string, v ...interface{}) {
+func (l *Loggable) Debugf(format string, v ...interface{}) {
 	l.Logger.Output(LogDebug, format, v...)
 }
 
-func (l Loggable) Errorf(format string, v ...interface{}) {
+func (l *Loggable) Errorf(format string, v ...interface{}) {
 	l.Logger.Output(LogError, format, v...)
 }
 
-func (l Loggable) Infof(format string, v ...interface{}) {
+func (l *Loggable) Infof(format string, v ...interface{}) {
 	l.Logger.Output(LogInfo, format, v...)
 }
 
-func (l Loggable) Warningf(format string, v ...interface{}) {
+func (l *Loggable) Warningf(format string, v ...interface{}) {
 	l.Logger.Output(LogWarning, format, v...)
 }
